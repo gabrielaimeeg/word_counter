c0055f78382741ba863873c78526523671a567b0
Gabriela Aimée <gabrielaimeeg@gmail.com>
2021-12-02 03:53:31 -0300

    Removing dependencys not used.

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,14 +1,36 @@
-
 $("#btnCount").click(function () {
     let phase = $("#txtWords").val();
 
-if (isTheTextValid(phase)) {
-    phase = replaceAllowedPunctuation(phase);
-    $("#pResult").text("You've typed " + countWords(phase) + " words!")
+if (isPhaseValid(phase)) {
+    let phaseCorrected = replaceAllowedPunctuation(phase);
+    showResult(phaseCorrected);
 }
 
 });
 
+function showResult(phase) {
+    $("#pResult").text("You've typed " + countWords(phase) + " words!");
+}
+
+function isPhaseValid(phase) {
+    return isTheTextValid(phase);
+}
+
+function replaceAllowedPunctuation(phase) {
+    return phase.replaceAll(",", "")
+        .replaceAll(".", "")
+            .replaceAll(";", "")
+                .replaceAll("!", "")
+                    .replaceAll("?", "")
+                        .replaceAll(":", "")
+                            .replaceAll("'", "")
+                                .replaceAll("(", "")
+                                    .replaceAll(")", "");
+}
+
+function countWords(str) {
+    return str.trim().split(/\s+/).length;
+}
 function isTheTextValid(phase) {
     if(isTextAreaBlack(phase)){
         $("#pResult").text("You need to enter something first.")
@@ -29,7 +51,7 @@ function isTheTextValid(phase) {
 }
 
 function isTextAreaBlack(phase){
-    return phase === null || phase ==='' || phase === 'undefined';
+    return phase === null || phase === '' || phase === "" || phase === 'undefined';
 }
 
 function isAnyOfTheWordsNumbers(phase){
@@ -39,20 +61,4 @@ function isAnyOfTheWordsNumbers(phase){
 function isAnyOfTheWordsSpecialChars(phase){
     let specialCharsNotAllowed = /[`#$%^&*_+\-=\[\]{}"\\|<>\/~]/;
     return specialCharsNotAllowed.test(phase);
-}
-
-function replaceAllowedPunctuation(phase) {
-    return phase.replaceAll(",", "")
-        .replaceAll(".", "")
-            .replaceAll(";", "")
-                .replaceAll("!", "")
-                    .replaceAll("?", "")
-                        .replaceAll(":", "")
-                            .replaceAll("'", "")
-                                .replaceAll("(", "")
-                                    .replaceAll(")", "");
-}
-
-function countWords(str) {
-    return str.trim().split(/\s+/).length;
 }
